efea2fba45619130dc2f099c31246490ea2310ca
Marleen <marleen.traas@gmail.com>
2021-02-23 23:07:58 +0100

    munchkin beginnings

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -12,6 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public static FragmentManager fragmentManager;
     boolean exiting = false;
 
